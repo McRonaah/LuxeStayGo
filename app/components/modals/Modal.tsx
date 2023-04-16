3074c9ae4865a4c7e3c6db5e7260a9f331ae930b
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-17 00:53:01 +0300

    :thread: setting effect hooks

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useCallback, useEffect, useState } from "react";
 interface ModalProps {
     isOpen?: boolean;
     onClose: () => void;
@@ -23,6 +26,43 @@ const Modal: React.FC<ModalProps> = ({
     secondaryAction,
     secondaryActionLabel
 }) => {
+    const [showModal, setShowModal] = useState(isOpen);
+
+    useEffect(() => {
+      setShowModal(isOpen);
+    }, [isOpen]);
+  
+    const handleClose = useCallback(() => {
+      if (disabled) {
+        return;
+      }
+    
+      setShowModal(false);
+      setTimeout(() => {
+        onClose();
+      }, 300)
+    }, [ disabled, onClose]);
+  
+    const handleSubmit = useCallback(() => {
+      if (disabled) {
+        return;
+      }
+  
+      onSubmit();
+    }, [onSubmit, disabled]);
+  
+    const handleSecondaryAction = useCallback(() => {
+      if (disabled || !secondaryAction) {
+        return;
+      }
+  
+      secondaryAction();
+    }, [secondaryAction, disabled]);
+  
+    if (!isOpen) {
+      return null;
+    }
+  
   return (
     <div>Modal</div>
   );
