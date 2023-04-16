e5ee3d457a2adc04156884809ad849aab3d516dd
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-17 00:48:11 +0300

    :necktie: added interface props

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,8 +1,31 @@
-
-const Modal = () => {
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryActionLabel?: string;
+  }
+  
+const Modal: React.FC<ModalProps> = ({
+    isOpen, 
+    onClose, 
+    onSubmit, 
+    title, 
+    body, 
+    actionLabel, 
+    footer, 
+    disabled,
+    secondaryAction,
+    secondaryActionLabel
+}) => {
   return (
     <div>Modal</div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
