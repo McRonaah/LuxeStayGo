c6bc3732fe4e20966e70f172d5e2392cda0a8de6
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-18 01:02:50 +0300

    :building_construction: updating register form

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
-import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
       <ClientOnly>
-        <Modal actionLabel='Submit' title='Ronald' isOpen/>
+          <RegisterModal/>
           <Navbar />
         </ClientOnly>
           {/* <div className="pb-20 pt-28"> */}
