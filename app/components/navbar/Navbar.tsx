532878735916155bf11fcb1252a51882795497c2
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-19 01:32:56 +0300

    :construction_worker: updated user types

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,11 +5,10 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-// import { SafeUser } from "@/app/types";
-import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({
