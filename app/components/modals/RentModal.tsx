005b9e72b5bebc3d2cc547237d30970894412c9f
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-21 19:47:24 +0300

    :technologist: :fixes: counter routing to rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -16,10 +16,11 @@ import {
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation';
 
-// import Counter from "../inputs/Counter";
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from "../inputs/CountrySelect";
 import { categories } from '../navbar/Categories';
+
+import Counter from "../inputs/Counter";
 // import ImageUpload from '../inputs/ImageUpload';
 import Input from '../inputs/Input';
 enum STEPS {
@@ -181,26 +182,26 @@ const RentModal = () => {
           title="Share some basics about your place"
           subtitle="What amenitis do you have?"
         />
-        {/* <Counter 
+        <Counter 
           onChange={(value) => setCustomValue('guestCount', value)}
           value={guestCount}
           title="Guests" 
           subtitle="How many guests do you allow?"
-        /> */}
+        />
         <hr />
-        {/* <Counter 
+        <Counter 
           onChange={(value) => setCustomValue('roomCount', value)}
           value={roomCount}
           title="Rooms" 
           subtitle="How many rooms do you have?"
-        /> */}
+        />
         <hr />
-        {/* <Counter 
+        <Counter 
           onChange={(value) => setCustomValue('bathroomCount', value)}
           value={bathroomCount}
           title="Bathrooms" 
           subtitle="How many bathrooms do you have?"
-        /> */}
+        />
       </div>
     )
   }
