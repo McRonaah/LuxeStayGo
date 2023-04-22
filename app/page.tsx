9052e36276438177c606eb97a83104e498acb311
McRonaah <ronaldcheruiyot342@gmail.com>
2023-04-22 15:01:30 +0300

    :arrow_up: updating listing to page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "@/app/components/Container";
+
 function Home() {
   return (
- <div className="text-rose-500 text-2xl">Ronald Aibnb!</div>
+    <ClientOnly>
+    <Container>
+      <div 
+        className="
+          pt-24
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {/* {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))} */}
+        my list
+      </div>
+    </Container>
+  </ClientOnly>
 );
 };
 
