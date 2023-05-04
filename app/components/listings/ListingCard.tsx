a2f5c2a46c5395b776a04a37b66e1f97a91b12b7
McRonaah <ronaldcheruiyot342@gmail.com>
2023-05-04 22:38:28 +0300

    :ambulance: :feet: listing card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -5,40 +5,40 @@ import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
 
-// import useCountries from "@/app/hooks/useCountries";
-// import { 
-//   SafeListing, 
-//   SafeReservation, 
-//   SafeUser 
-// } from "@/app/types";
+import useCountries from "@/app/hooks/useCountries";
+import { 
+  SafeListing, 
+  SafeReservation, 
+  SafeUser 
+} from "@/app/types";
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
-//   data: SafeListing;
-//   reservation?: SafeReservation;
+  data: SafeListing;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
   actionId?: string;
-//   currentUser?: SafeUser | null
+  currentUser?: SafeUser | null
 };
 
 const ListingCard: React.FC<ListingCardProps> = ({
-//   data,
-//   reservation,
+  data,
+  reservation,
   onAction,
   disabled,
   actionLabel,
   actionId = '',
-//   currentUser,
+  currentUser,
 }) => {
   const router = useRouter();
-//   const { getByValue } = useCountries();
+  const { getByValue } = useCountries();
 
-//   const location = getByValue(data.locationValue);
+  const location = getByValue(data.locationValue);
 
   const handleCancel = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -51,24 +51,24 @@ const ListingCard: React.FC<ListingCardProps> = ({
     onAction?.(actionId)
   }, [disabled, onAction, actionId]);
 
-//   const price = useMemo(() => {
-//     if (reservation) {
-//       return reservation.totalPrice;
-//     }
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
 
-//     return data.price;
-//   }, [reservation, data.price]);
+    return data.price;
+  }, [reservation, data.price]);
 
-//   const reservationDate = useMemo(() => {
-//     if (!reservation) {
-//       return null;
-//     }
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
   
-//     const start = new Date(reservation.startDate);
-//     const end = new Date(reservation.endDate);
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
 
-//     return `${format(start, 'PP')} - ${format(end, 'PP')}`;
-//   }, [reservation]);
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+  }, [reservation]);
 
   return (
     <div 
