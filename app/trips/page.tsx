877950f16fbe64a5613acfa8f258bf88b16202f7
McRonaah <ronaldcheruiyot342@gmail.com>
2023-05-07 10:44:55 +0300

    :heavy_minus_sign: fixes trips client content

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -4,7 +4,7 @@ import ClientOnly from "@/app/components/ClientOnly";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getReservations from "@/app/actions/getReservations";
 
-// import TripsClient from "./TripsClient";
+import TripsClient from "./TripsClient";
 
 const TripsPage = async () => {
   const currentUser = await getCurrentUser();
